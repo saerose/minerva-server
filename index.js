c77353038d0710e8f9ffde14e24a7ced407e29e8
Sae Rose <sbarcelocompte@gmail.com>
2018-09-24 10:54:01 +0200

    chore: cors added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
+const cors = require('koa-cors')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/minerva');
+mongoose.connect('mongodb://localhost:27017/minerva', { useNewUrlParser: true });
 
 const app = new Koa()
 const router = require ('./router')
@@ -10,6 +11,7 @@ const router = require ('./router')
 
 app
   .use(bodyparser())
+  .use(cors())
   .use(router.routes())
   .use(router.allowedMethods())
   .listen(3001, () => console.log('App listening to port 3001 🚀'))
