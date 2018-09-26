4f7775287d44d5f13c7fd8c99c270192892babc2
saerose <sbarcelocompte@gmail.com>
2018-09-26 12:32:48 +0200

    feat: addBookToLibrary works

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -38,19 +38,44 @@ module.exports = {
   },
 
   addBookToLibrary: async (ctx) => {
+    let insertBook = {}
+    insertBook = {
+      title: ctx.request.body.book.volumeInfo.title,
+      author: ctx.request.body.book.volumeInfo.authors,
+      bookCover: ctx.request.body.book.volumeInfo.imageLinks.thumbnail,
+      genre: ctx.request.body.book.volumeInfo.categories
+      // reviews: no reviews available in google api
+    }
+    const newBook = new Book(insertBook)
+    console.log(ctx.params.id)
+    console.log(ctx.params.book)
+    const user = await User.findOne({ id: ctx.params.id.toString() })
+    console.log(user)
+    const updated = await User.updateOne({ id: ctx.params.id.toString() }, {books: [ ...user.books, newBook ] })
+      .catch(console.error)
+    if (updated) {
+      ctx.status = 201
+      ctx.body = user
+    } else {
+      ctx.status = 500
+    }
+  }
+}
+
+
     /**
      * { book:
-   { kind: 'books#volume',
-     id: 'fnQ9mGMH15oC',
-     etag: 'dCj5gLLC9Fg',
-     selfLink: 'https://www.googleapis.com/books/v1/volumes/fnQ9mGMH15oC',
-     volumeInfo:
+  { kind: 'books#volume',
+    id: 'fnQ9mGMH15oC',
+    etag: 'dCj5gLLC9Fg',
+    selfLink: 'https://www.googleapis.com/books/v1/volumes/fnQ9mGMH15oC',
+    volumeInfo:
       { title: 'Cosmetología de Harry',
         authors: [Array],
         publisher: 'Ediciones Díaz de Santos',
         publishedDate: '1990',
         description:
-         'La utilidad de la COSMETOLOGÍA DE HARRY, como libro de consulta a la vez completo y conciso, está corroborada por su continua popularidad en todas las partes del mundo: estamos convencidos de que, una vez más, esta séptima edición hallará una calurosa acogida. INDICE: La piel. Irritación y sensibilización de la piel. Nutrición y control hormonal de la piel. Cremas cutáneas. Astringentes y tónicos de la piel. Cremas protectoras y limpiadoras de las manos. Preparados para el baño. Productos cutáneos para bebés. Productos cutáneos para jóvenes. Antiperspirantes y desodorantes. Depilatorios. Preparados para el afeitado. Preparados para los pies. Productos protectores solares. Decolorantes o aclaradores de la piel. Mascarillas y máscaras faciales. Polvos y maquillaje facial. Las uñas. Preparados de manicura. El pelo. Champúes. Lociones y aerosoles fijadores y lacas capilares. Colorantes del cabello. El diente y la salud bucal, etc.',
+        'La utilidad de la COSMETOLOGÍA DE HARRY, como libro de consulta a la vez completo y conciso, está corroborada por su continua popularidad en todas las partes del mundo: estamos convencidos de que, una vez más, esta séptima edición hallará una calurosa acogida. INDICE: La piel. Irritación y sensibilización de la piel. Nutrición y control hormonal de la piel. Cremas cutáneas. Astringentes y tónicos de la piel. Cremas protectoras y limpiadoras de las manos. Preparados para el baño. Productos cutáneos para bebés. Productos cutáneos para jóvenes. Antiperspirantes y desodorantes. Depilatorios. Preparados para el afeitado. Preparados para los pies. Productos protectores solares. Decolorantes o aclaradores de la piel. Mascarillas y máscaras faciales. Polvos y maquillaje facial. Las uñas. Preparados de manicura. El pelo. Champúes. Lociones y aerosoles fijadores y lacas capilares. Colorantes del cabello. El diente y la salud bucal, etc.',
         industryIdentifiers: [Array],
         readingModes: [Object],
         pageCount: 1062,
@@ -64,14 +89,14 @@ module.exports = {
         imageLinks: [Object],
         language: 'es',
         previewLink:
-         'http://books.google.es/books?id=fnQ9mGMH15oC&printsec=frontcover&dq=isbn:9788487189388&hl=&cd=1&source=gbs_api',
+        'http://books.google.es/books?id=fnQ9mGMH15oC&printsec=frontcover&dq=isbn:9788487189388&hl=&cd=1&source=gbs_api',
         infoLink:
-         'http://books.google.es/books?id=fnQ9mGMH15oC&dq=isbn:9788487189388&hl=&source=gbs_api',
+        'http://books.google.es/books?id=fnQ9mGMH15oC&dq=isbn:9788487189388&hl=&source=gbs_api',
         canonicalVolumeLink:
-         'https://books.google.com/books/about/Cosmetolog%C3%ADa_de_Harry.html?hl=&id=fnQ9mGMH15oC' },
-     saleInfo:
+        'https://books.google.com/books/about/Cosmetolog%C3%ADa_de_Harry.html?hl=&id=fnQ9mGMH15oC' },
+    saleInfo:
       { country: 'ES', saleability: 'NOT_FOR_SALE', isEbook: false },
-     accessInfo:
+    accessInfo:
       { country: 'ES',
         viewability: 'PARTIAL',
         embeddable: true,
@@ -80,24 +105,10 @@ module.exports = {
         epub: [Object],
         pdf: [Object],
         webReaderLink:
-         'http://play.google.com/books/reader?id=fnQ9mGMH15oC&hl=&printsec=frontcover&source=gbs_api',
+        'http://play.google.com/books/reader?id=fnQ9mGMH15oC&hl=&printsec=frontcover&source=gbs_api',
         accessViewStatus: 'SAMPLE',
         quoteSharingAllowed: false },
-     searchInfo:
+    searchInfo:
       { textSnippet:
-         'La utilidad de la COSMETOLOGÍA DE HARRY, como libro de consulta a la vez completo y conciso, está corroborada por su continua popularidad en todas las partes del mundo: estamos convencidos de que, una vez más, esta séptima edición ...' } } }
+        'La utilidad de la COSMETOLOGÍA DE HARRY, como libro de consulta a la vez completo y conciso, está corroborada por su continua popularidad en todas las partes del mundo: estamos convencidos de que, una vez más, esta séptima edición ...' } } }
      */
-    const newBook = new Book(ctx.request.body.book)
-    console.log(ctx.params.id)
-    const user = await User.findOne({ id: ctx.params.id.toString() })
-    console.log(user)
-    // const updated = await User.updateOne({ id: ctx.params.id.toString() }, {books: [ ...user.books, newBook ] })
-    //   .catch(console.error)
-    if (updated) {
-      ctx.status = 201
-      ctx.body = user
-    } else {
-      ctx.status = 500
-    }
-  }
-}
